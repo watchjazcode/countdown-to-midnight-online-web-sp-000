55625e728995b891a1e568ae29af787fc405f324
Jazmin M <jazminmerckel@gmail.com>
2019-09-23 07:08:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown(number)
-  number = 8
-  while number > 1
+  number = 10
+  until number = 1
     puts "#{number} SECOND(S)!"
     number -= 1
   return "HAPPY NEW YEAR!"
